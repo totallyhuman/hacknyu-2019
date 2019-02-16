da11276ed0d986068f3140906a52e4efb5b246a5
totallyhuman <bsumantb@gmail.com>
2019-02-16 14:30:57 -0500

    Beautify opency.py

--- server/opencv.py
+++ server/opencv.py
@@ -26,8 +26,10 @@ def fourPointTransform(image, points):
     rect = pointOrder(points)
     (tl, tr, br, bl) = rect
 
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2)) # Width at bottom
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2)) # width at top
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) +
+                     + ((br[1] - bl[1]) ** 2))  # Width at bottom
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) +
+                     + ((tr[1] - tl[1]) ** 2))  # width at top
     maxWidth = max(int(widthA), int(widthB))
 
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
@@ -49,13 +51,15 @@ def fourPointTransform(image, points):
 
     return warped
 
+
 def arcLength2(con):
     return cv2.arcLength(con, True)
+
 
 def do_things(filePath):
 
     image = cv2.imread(filePath)
-    ratio = image.shape[0]/600
+    ratio = image.shape[0] / 600
     orig = image.copy()
     image = imutils.resize(image, height=600)
 
@@ -63,38 +67,35 @@ def do_things(filePath):
     # gray = cv2.GaussianBlur(image, (5, 5), 0)
     edged = cv2.Canny(gray, 125, 150)
 
-    cv2.imshow("ed",edged)
+    cv2.imshow("ed", edged)
     cv2.waitKey(0)
 
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST,
+                            cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sorted(cnts, key=arcLength2, reverse=True)[:20]
 
     if(len(cnts) < 1):
         print("Not a single contour detected")
-    
 
     for c in cnts:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
 
         if len(approx) == 4:
             screenCnt = approx
             break
         if len(approx) < 4:
             print("under min amount of vertexs, attempting to continue")
-        
-        boundingBox = cv2.boundingRect(approx)
 
-        
+        boundingBox = cv2.boundingRect(approx)
 
         approx = np.array(
             [
-                [[boundingBox[0] + boundingBox[2],boundingBox[1]]], #tr
-                [[boundingBox[0], boundingBox[1]]], #tl
-                [[boundingBox[0], boundingBox[1] + boundingBox[3]]], #bl
-                [[boundingBox[0] + boundingBox[2], boundingBox[1] + boundingBox[3]]] #br
+                [[boundingBox[0] + boundingBox[2], boundingBox[1]]],  # tr
+                [[boundingBox[0], boundingBox[1]]],  # tl
+                [[boundingBox[0], boundingBox[1] + boundingBox[3]]],  # bl
+                [[boundingBox[0] + boundingBox[2], boundingBox[1] + boundingBox[3]]]  # br
             ],
             dtype=int
         )
