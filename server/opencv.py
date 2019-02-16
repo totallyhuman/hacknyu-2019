ae43cc3c58365768351fe044f0c440ce91189f30
BullTheRockLord <timrmiles@gmail.com>
2019-02-16 02:52:22 -0500

    Changed opencv to work with others

--- server/opencv.py
+++ server/opencv.py
@@ -22,9 +22,10 @@ def pointOrder(pts):
 def fourPointTransform(image, points):
     rect = pointOrder(points)
     (tl, tr, br, bl) = rect
+
     
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2)) #Width at bottom
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - br[1]) ** 2)) # width at top
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2)) # width at top
     maxWidth = max(int(widthA), int(widthB))
 
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
@@ -50,42 +51,39 @@ def fourPointTransform(image, points):
     return warped;
 
 
-image = cv2.imread("image.jpg")
-ratio = image.shape[0]/600
-orig = image.copy()
-image = imutils.resize(image, height=600)
+def do_things(filePath, outputPath):
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(image, (5,5), 0)
-edged = cv2.Canny(gray, 75, 200)
+    image = cv2.imread(filepath)
+    ratio = image.shape[0]/600
+    orig = image.copy()
+    image = imutils.resize(image, height=600)
 
-
-
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key = cv2.contourArea, reverse=True)[:5]
-
-for c in cnts:
-    peri = cv2.arcLength(c,True)
-    approx = cv2.approxPolyDP(c, 0.02*peri,True)
-
-    if len(approx) == 4:
-        screenCnt = approx
-        break
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(image, (5,5), 0)
+    edged = cv2.Canny(gray, 75, 200)
 
 
 
-warped = fourPointTransform(orig, screenCnt.reshape(4,2) * ratio)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse=True)[:5]
 
-warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-T = threshold_local(warped, 11, offset=10, method = "gaussian")
+    for c in cnts:
+        peri = cv2.arcLength(c,True)
+        approx = cv2.approxPolyDP(c, 0.02*peri,True)
 
-#warped = (warped > T).astype("uint8") * 255
+        if len(approx) == 4:
+            screenCnt = approx
+            break
 
-print("Now we have our new image")
-cv2.imshow("Original", imutils.resize(orig, height = 650))
-cv2.imshow("New", imutils.resize(warped, height= 650))
 
-cv2.imwrite("Warped.jpg", warped)
 
-cv2.waitKey(0)
+    warped = fourPointTransform(orig, screenCnt.reshape(4,2) * ratio)
+
+    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+
+    #warped = (warped > T).astype("uint8") * 255
+
+    cv2.imwrite(outputPath, warped)
+
+  
