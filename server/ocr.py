37eef94ecfcfaf2fc10196cd8cc854b399d7ce7d
Daniel <hello@danielwang.me>
2019-02-15 22:43:50 -0500

    feat: add ocr

---
+++ server/ocr.py
@@ -0,0 +1,23 @@
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                     for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+              
+if __name__ == "__main__":
+    detect_text_uri(
+        "https://catgirl.pw/Dlf97s.jpg")
