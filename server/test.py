1ab92b2bb35d4dc6fd5550d37b564507a8f587da
Daniel <hello@danielwang.me>
2019-02-17 03:31:22 -0500

    fix(remove jquery dependency):

--- server/test.py
+++ server/test.py
@@ -6,8 +6,9 @@ import opencv
 
 if __name__ == "__main__":
     img_data = requests.get(sys.argv[1]).content
-    with open('image.jpg', 'wb') as handler:
+    with open('/var/www/html/images/image.jpg', 'wb') as handler:
         handler.write(img_data)
-    ocr_image = ocr.analyze_image(opencv.do_things("image.jpg"))
-    print(content_classification.classify(ocr.detect_text(ocr_image)))
-    print(ocr.calculate_total(ocr_image))
+    ocr_image = ocr.analyze_image(
+        opencv.do_things("/var/www/html/images/image.jpg"))
+    print(content_classification.classify(ocr.detect_text(ocr.analyze_image(ocr_image))))
+    print(ocr.calculate_total(ocr.analyze_image(ocr_image)))
